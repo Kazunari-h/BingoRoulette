70aedd691cb2d08f2393b9f6de1f92c81a485af5
Kazunari-h <kazunario456@gmail.com>
2019-12-20 18:17:32 +0900

    抽選の修正

--- CreateNum.js
+++ CreateNum.js
@@ -1,14 +1,14 @@
 var numList = [];
 var displayNumList = [];
 var count = 1;
-var id ;
+var id;
 var audio = new Audio();
 audio.src = "drm.mp3";
 
 
-window.onload = function(){
-  start(75);
-  document.getElementById("btn").onclick = function(){
+window.onload = function () {
+  start(35);
+  document.getElementById("btn").onclick = function () {
     audio.play();
     document.getElementById("btn").disabled = true;
     roulette();
@@ -16,25 +16,25 @@ window.onload = function(){
 }
 
 function start(num) {
-  for(var i = 1 ; i <= num ; i++){
+  for (var i = 1; i <= num; i++) {
     numList.push(i);
   }
 }
 
 function displayNum() {
-  var id = setInterval(function(){
+  var id = setInterval(function () {
     var rdmNum = Math.floor(Math.random() * numList.length);
     document.getElementById("num").innerText = numList[rdmNum];
   }, 50);
-  setTimeout(function(){
+  setTimeout(function () {
     clearInterval(id);
     document.getElementById("btn").disabled = false;
     var rdmNum = Math.floor(Math.random() * numList.length);
-    displayNumList.unshift( numList[rdmNum]);
+    displayNumList.unshift(numList[rdmNum]);
     document.getElementById("num").innerHTML = numList[rdmNum]
-    document.getElementById("history").innerHTML = displayNumList.join( "<br>" );
-    numList.splice( rdmNum , 1 ) ;
-  },5200);
+    document.getElementById("history").innerHTML = displayNumList.join("<br>");
+    numList.splice(rdmNum, 1);
+  }, 3200);
 }
 
 function roulette() {
